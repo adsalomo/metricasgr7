679d23fbb4ba85def3667e5160db2a425396fff7
adsalomo <adsalomo@gmail.com>
2021-10-08 20:34:59 -0500

    cambios a la clase persona

--- Persona.java
+++ Persona.java
@@ -3,12 +3,22 @@ public class Persona {
 private String nombre;
 private int edad;
 
+public Persona() {}
+
 public String getNombre() {
 	return nombre;
 }
 
 public int getEdad() {
 	return edad;
+}
+
+public void setNombre(String nombre){ 
+this.nombre = nombre;
+}
+
+public void setEdad(int edad) {
+	this.edad = edad;
 }
 
 }
